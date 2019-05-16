638f8e979a5bd0db457d2cfe6d1596b71a0bbb88
sheik syed abthaheer <sheiksyedm@syncfusion.com>
2019-05-16 18:17:52 +0530

    samples added.

---
+++ CustomView/CustomView.iOS/CustomViewRenderer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CoreGraphics;
+using CustomView.iOS;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: Xamarin.Forms.ExportRenderer(typeof(CustomView.CustomView), typeof(CustomViewRenderer))]
+
+namespace CustomView.iOS
+{
+    class CustomViewRenderer : ViewRenderer
+    {
+        public override void Draw(CGRect rect)
+        {
+            base.Draw(rect);
+            var context = UIGraphics.GetCurrentContext();
+            var formsView = Element as CustomView;
+            if (formsView != null & formsView.IsDrawBackground)
+            {
+                context.SaveState();
+                context.SetFillColor(UIColor.Red.CGColor);
+                context.AddRect(rect);
+                context.DrawPath(CGPathDrawingMode.FillStroke);
+                context.RestoreState();
+            }
+        }
+
+        public new void Init()
+        {
+
+        }
+    }
+}
---
+++ CustomView/CustomView/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CustomView
+{
+    public partial class MainPage : ContentPage
+    {
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            customView.IsVisible = !customView.IsVisible;
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class CustomView : View
+    {
+        public static readonly BindableProperty IsDrawBackgroundProperty =
+             BindableProperty.Create("IsDrawBackground", typeof(bool), typeof(CustomView), null, BindingMode.Default, null, null);
+        public bool IsDrawBackground
+        {
+            get { return (bool)GetValue(IsDrawBackgroundProperty); }
+            set { SetValue(IsDrawBackgroundProperty, value); }
+        }
+
+    }
+}
